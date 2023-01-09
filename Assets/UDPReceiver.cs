c826ae6aae00fa26a3654efc48544c7fa499e08b
rin4046 <rin@rinrin.me>
2023-01-10 06:31:48 +0900

    init

---
+++ Assets/UDPReceiver.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class UDPReceiver : MonoBehaviour
+{
+  private UdpClient receiver;
+  private Vector3 force = new(0, 0, 0);
+
+  private Rigidbody rb;
+
+  void Start()
+  {
+    rb = this.GetComponent<Rigidbody>();
+    receiver = new(54321);
+
+    Task.Run(() =>
+    {
+      while (true)
+      {
+        IPEndPoint ep = null;
+        byte[] msg = receiver.Receive(ref ep);
+
+        // MemoryMarshalでbyte[]をfloat[]に
+        Span<float> converted = MemoryMarshal.Cast<byte, float>(msg).ToArray();
+
+        force.x = converted[0];
+        force.z = converted[1];
+      }
+    });
+  }
+
+  void Update()
+  {
+    rb.AddForce(force);
+  }
+}
